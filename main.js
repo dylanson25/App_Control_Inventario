9c652e48b151f541a6082dad1d7b57e5a9406ca8
DYLAN VILLARREAL TOSCANO <54459436+dylanson25@users.noreply.github.com>
2020-10-26 16:16:24 -0600

    avance

---
+++ main.js
@@ -0,0 +1,10 @@
+class Producto{
+    constructor({codigo, nombre, desc, cantidad, costo}){
+        this._productos = new Array()
+        this._codigo = codigo
+        this._nombre = nombre
+        this._desc = desc
+        this._cantidad = cantidad
+        this._costo = costo
+    }
+}
