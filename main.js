fec7d8ea251f1f7a15b6d00f111b85b1b9c20213
DYLAN VILLARREAL TOSCANO <54459436+dylanson25@users.noreply.github.com>
2020-10-26 17:29:03 -0600

    agregar

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-class Producto{
-    constructor({codigo, nombre, desc, cantidad, costo}){
-        this._productos = new Array()
+var code = document.getElementById("iptCod")
+var nom = document.getElementById("iptNom")
+var desc = document.getElementById("iptDes")
+var cant = document.getElementById("iptCant")
+var cost = document.getElementById("iptCost")
+class Producto {
+    constructor(codigo, nombre, desc, cantidad, costo) {
         this._codigo = codigo
         this._nombre = nombre
         this._desc = desc
@@ -8,3 +12,21 @@ class Producto{
         this._costo = costo
     }
 }
+class Almacen {
+    constructor() {
+        this._productos = new Array()
+    }
+    addProduct(Producto) {
+        let pos = this._productos.length
+        if (pos < 20) {
+            this._productos[pos] = Producto
+            return true
+        }
+    }
+    mostrar() {
+        console.log(this._productos[0])
+    }
+}
+btnAgregar.addEventListener('click', () => {
+    let nuevo = new Producto(code, nom, desc, cant, cost)
+})
